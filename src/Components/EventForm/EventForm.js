d4c1c705bb996934519db4894ef626dd7a9e709b
Austin Krause <aus.kr4@gmail.com>
2023-07-15 11:19:39 -0500

    background colors

--- src/Components/EventForm/EventForm.js
+++ src/Components/EventForm/EventForm.js
@@ -16,7 +16,7 @@ function EventForm({ onAdd }) {
   };
 
   return (
-    <div className="eventFormContainer p-6 rounded stickyForm">
+    <div className="eventFormContainer p-6 stickyForm">
       <form onSubmit={handleSubmit} className="eventForm space-y-4">
         <div className="inline items-center space-x-4">
           <label>Event Type:</label>
